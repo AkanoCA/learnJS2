4d55b24d69f58b5d1a89df438ac291b4e79083b0
AkanoCA <akanoca@yahoo.com>
2020-01-18 22:13:20 +0300

    First task

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 'use strict';
 
+let money = prompt("Ваш бюджет на месяц?", "1000");
+let time = prompt("Введите дату в формет YYYY-MM-DD", "1125-11-28");
 
+
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
+
+for ( let i = 0; i < 2; i++){
+    let item = prompt("Введите обязательную статью рассходов в этом месяце", "Еда");
+    let itemCost = prompt("Во сколько обойдется?", "5000");
+    appData.expenses[item] = itemCost;
+}
+
+
+alert(+appData.budget / 30);
